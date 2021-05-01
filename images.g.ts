6669e6f51b925debcca3e89eeedd89d2d79ffa29
Kevin McDonnell <kevin@mcd79.com>
2021-05-01 10:16:47 +0100

    Added flask and bunny

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Bunny-Rabbit-collin_sparkles":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . d e e e e d . . . . 
+. . . . . d e 8 e 8 e e d . . . 
+. . . . d e e e f e e e . d . . 
+. . . d . . e e e e e . . . d . 
+. . d . . . . e e e . . . . . d 
+. d . . . e e e e e e e . . . d 
+. d . . e e e e e e e e e . . d 
+. d . . e e e e e e e e e . . . 
+. . . . . e e e e e e e . . . . 
+. . . . e e e e e e e e e . . . 
+. . f f f f f f f f f f f f . . 
+. . f f f f f f f f f f f f . . 
+`;
+            case "image2":
+            case "ChocFlask-kevmcdonk":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 9 9 9 9 . . . . . . 
+. . . . . . . 9 9 . . . . . . . 
+. . . . . . . 9 9 . . . . . . . 
+. . . . . . . 9 9 . . . . . . . 
+. . . . . . 9 9 9 9 . . . . . . 
+. . . . . 9 9 9 9 9 9 . . . . . 
+. . . . 9 9 9 9 9 9 9 9 . . . . 
+. . . 9 9 e e 9 9 9 e e 9 . . . 
+. . 9 9 9 e e 9 9 9 e e 9 9 . . 
+. 9 9 e e 9 e e 9 e e 9 e e 9 . 
+. 9 9 e e 9 e e 9 e e 9 e e 9 . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
